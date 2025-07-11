917a9001af376ae7666031985dc609d369726495
Muzamil <muzamilzubair987654@gmail.com>
2025-07-11 22:10:20 +0500

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// import Hook from "./Hook.jsx";
+import Hook from "./Hook.jsx";
 import User from "./User.jsx";
 function App() {
   return (
     <div>
       <h1 className="bg-amber-300 m-2.5">React Hook</h1>
-      {/* <Hook /> */}
+      <Hook />
       <User />
     </div>
   );
